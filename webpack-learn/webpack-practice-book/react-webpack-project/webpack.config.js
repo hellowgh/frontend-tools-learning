07c4a950516ce64058f3373e3dbb78800d6ab6af
w30021754 <wangguohua6@huawei.com>
2024-03-01 07:46:57 +0800

    添加环境变量

--- webpack-learn/webpack-practice-book/react-webpack-project/webpack.config.js
+++ webpack-learn/webpack-practice-book/react-webpack-project/webpack.config.js
@@ -1,8 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// 注意！脚本中执行set NODE_ENV=production && webpack build，会使得环境production后面有一个空格，因此要trim一下
+const isProduction = process.env.NODE_ENV.trim() === 'production';
+
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: isProduction ? 'production' : 'development',
     devServer: {
       open: true,
       port: 3000
