cc3635825494de045ce3e9fbad497dd47d089b00
w30021754 <wangguohua6@huawei.com>
2024-03-01 08:50:11 +0800

    添加ts支持

---
+++ webpack-learn/webpack-practice-book/react-webpack-project/src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+/**
+ * 支持并发渲染
+ * 减少卡顿
+ * 更好的错误处理
+ * 统一客户端和服务端渲染方式
+ */
+import { createRoot } from 'react-dom/client'
+
+type Message = {
+    title: string;
+    body: string;
+}
+
+const msg: Message = {
+    title: 'title',
+    body: 'body'
+}
+
+const App = () => (
+    <>
+        <h1>{msg.title}</h1>
+        <div>{msg.body}</div>
+    </>
+)
+
+const root = createRoot(
+    document.getElementById('root')
+)
+
+root.render(<App/>)
+
--- webpack-learn/webpack-practice-book/react-webpack-project/webpack.config.js
+++ webpack-learn/webpack-practice-book/react-webpack-project/webpack.config.js
@@ -6,12 +6,18 @@ const isProduction = process.env.NODE_ENV.trim() === 'production';
 module.exports = () => {
   return {
     mode: isProduction ? 'production' : 'development',
+    entry: './src/index.tsx',
     devServer: {
       open: true,
       port: 3000
     },
     module: {
       rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: 'ts-loader'
+        },
         {
           test: /\.jsx?$/,
           exclude: /node_modulses/,
@@ -26,7 +32,7 @@ module.exports = () => {
       ]
     },
     resolve: {
-      extensions: ['.js', '.jsx'] // 让jsx文件可以被Resolver自动识别（注意是 .js 非 js）
+      extensions: ['.js', '.jsx', 'tsx'] // 让jsx文件可以被Resolver自动识别（注意是 .js 非 js）
     },
     plugins: [
       new HtmlWebpackPlugin({
