0af49d956906c2d446aa14651dcec2da1c99cee8
w30021754 <wangguohua6@huawei.com>
2024-03-02 09:13:14 +0800

    完善配置文件

--- webpack-learn/webpack-practice-book/vue-webapck-manual-project/webpack.config.js
+++ webpack-learn/webpack-practice-book/vue-webapck-manual-project/webpack.config.js
@@ -1,8 +1,42 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import {CleanWebpackPlugin} from 'clean-webpack-plugin';
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+  const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+  const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+  const path = require('path');
 
-module.exports = () => ({
-  plugins: [
-    new HtmlWebpackPlugin()
-  ]
-})
+  const envMode = process.env.NODE_ENV.trim();
+  const isProduction = envMode === 'production';
+
+  module.exports = () => ({
+    mode: envMode,
+    devServer: {
+      port: 3000,
+      open: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: 'ts-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
+          ]
+        }
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'vue-webpack-manual-project',
+        template: './public/index.html'
+      }),
+      isProduction && new MiniCssExtractPlugin({
+        chunkFilename: '[name].[contenthash:8].chunk.css',
+        filename: '[name].[contenthash:8].css'
+      })
+    ].filter(Boolean)
+  })
