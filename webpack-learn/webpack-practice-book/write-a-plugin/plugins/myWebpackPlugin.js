bd0f6e94742d703bf0303f450bea2d2b3a4a968f
w30021754 <wangguohua6@huawei.com>
2024-03-07 20:04:56 +0800

    完成一个简易plugin

--- webpack-learn/webpack-practice-book/write-a-plugin/plugins/myWebpackPlugin.js
+++ webpack-learn/webpack-practice-book/write-a-plugin/plugins/myWebpackPlugin.js
@@ -2,11 +2,18 @@ const { validate } = require('schema-utils');
 const schema = require('./schema/schema')
 
 class MyWebpackPlugin {
-  constructor(options) {
-    // 校验使用插件时传入的options是否符合schema
-    validate(schema, options);
+  static defaultOptions = {
+    name: 'My Webpack Plugin'
+  }
 
-    this.options = options;
+  constructor(options = {}) {
+    // 校验插件传入的options是否符合schema
+    validate(schema, options, {
+      name: 'My Webpack Plugin',
+      baseDataPath: 'options',
+    });
+
+    this.options = { ...MyWebpackPlugin.defaultOptions, ...options };
   }
 
   apply(compiler) {
@@ -17,3 +24,10 @@ class MyWebpackPlugin {
 }
 
 module.exports = MyWebpackPlugin;
+
+compiler.hooks.someHook.tapPromise('MyPlugin', async (arg) => {
+  // 处理异步逻辑并返回 Promise
+  setTimeout(() => {
+    return Promise.resolve();
+  })
+});
